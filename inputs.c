84accbf931cdc79a922fc11ee96ad9d8fcdb1cba
dhyey0 <dhyeypatel2907@gmail.com>
2025-03-22 14:18:06 +0530

    CFG added in inputs.c

--- inputs.c
+++ inputs.c
@@ -9,3 +9,59 @@ select age, salary from Employees with condition department = "MDES"
 update salary to 55000 in Employees
 update department to "LFI" with condition name = "HIL Patel" in Employees
 */
+
+
+/*'
+//THE FOLLOWING IS THE CFG 
+
+
+
+
+
+
+<commands> ::= <create_table_stmt>
+            | <insert_stmt>
+            | <delete_stmt>
+            | <update_stmt>
+            | <select_stmt>
+
+<create_table_stmt> ::= CREATE TABLE IDENTIFIER WITH <column_list>
+
+<column_list> ::= IDENTIFIER
+               | <column_list> ',' IDENTIFIER
+
+<insert_stmt> ::= ADD <column_list> WITH <value_list> TO IDENTIFIER
+
+<value_list> ::= STRING_LITERAL
+              | INT_LITERAL
+              | <value_list> ',' STRING_LITERAL
+              | <value_list> ',' INT_LITERAL
+
+<delete_stmt> ::= REMOVE FROM IDENTIFIER
+
+<update_stmt> ::= UPDATE IDENTIFIER SET IDENTIFIER '=' STRING_LITERAL
+
+<select_stmt> ::= FIND ALL FROM IDENTIFIER
+               | FIND <select_column_list> FROM IDENTIFIER
+
+<select_column_list> ::= IDENTIFIER
+                       | <select_column_list> ',' IDENTIFIER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       
+*/
